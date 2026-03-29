135b61f0285b4151985cea4efdb486370455b1b4
tvanlee <trongthptdqh@gmail.com>
2026-03-29 20:20:31 +0700

    update layout teacher cách xa

--- src/app/(teacher)/layout.tsx
+++ src/app/(teacher)/layout.tsx
@@ -15,7 +15,7 @@ export default function TeacherRouteLayout({
         <div className="flex flex-1 pt-16">
           <TeacherSidebar />
           <div className="flex-1 flex flex-col min-w-0">
-            <main className="p-4 md:p-6 pb-20 md:pb-6 w-full max-w-7xl mx-auto flex-1">
+            <main className="p-4 md:p-6 pb-20 md:pb-6 w-full flex-1">
               {children}
             </main>
             <BottomNav role="teacher" />
